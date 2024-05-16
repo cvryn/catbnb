d1291edecfe0b3bae4e4314f7fba8f0a61708ed0
caryn <ca.ryn.ono.w@gmail.com>
2024-05-16 11:13:42 -0700

    Updated migration files

--- backend/db/migrations/20240514215021-create-review-image.js
+++ backend/db/migrations/20240514215021-create-review-image.js
@@ -33,7 +33,7 @@ module.exports = {
         type: Sequelize.DATE,
         defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
       }
-    });
+    }, options);
   },
 
   async down(queryInterface, Sequelize) {
