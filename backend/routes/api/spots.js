841582052d90c0d3300ac53575468f2b0ca56eff
caryn <ca.ryn.ono.w@gmail.com>
2024-05-17 18:05:12 -0700

    Fixed an error in spot route

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -277,7 +277,7 @@ router.post("/:spotId/images", requireAuth, async (req, res) => {
   if (currentOwnerId === owner) {
     const { url, preview } = req.body;
     let newImg = await SpotImage.create({
-      where: { spotId: spotId },
+      where: { id: spotId },
       url,
       preview,
     });
